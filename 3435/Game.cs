5cd28f5bef42b3a1464cb7f4c95104a4c61f6deb
Danil011-BOSS <zakdanil07@gmail.com>
2026-04-14 09:15:30 +0300

    Использование обобщенного класса при типизации C#
    Использование обобщенного класса при типизации C#

---
+++ 3435/Game.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3435
+{
+    class Game<P>
+    {
+        private string company;
+        private P member;
+        public string Company
+        {
+            set
+            {
+                company = value;
+            }
+            get
+            {
+                return company;
+            }
+        }
+        public P Member
+        {
+            set
+            {
+                member = value;
+            }
+            get
+            {
+                return member;
+            }
+        }
+        public Game(string company, P member)
+        {
+            this.company = company;
+            this.member = member;
+        }
+    }
+}
---
+++ 3435/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3435
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Обобщенный класс как обобщенный тип");
+            Game<Person2<int>> gm1 = new Game<Person2<int>>("КГАСУ",
+            new Person2<int>("Иванов", 123));
+            Console.WriteLine("Организация участника {0}, фамилия {1},
+            информация { 2}
+            ", gm1.Company, gm1.Member.Name,
+gm1.Member.Info);
+        }
+    }
+}
